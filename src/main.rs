07948ae8b70ccdf554f919e37d7d5ad614342b56
AlexandreImperiale <alexandre.imperiale@gmail.com>
2018-03-18 19:05:34 +0100

    Adding routing commands.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 #[macro_use] extern crate rouille;
 
+fn handler(request: &rouille::Request) -> rouille::Response
+{
+    rouille::match_assets(&request.remove_prefix("/static").unwrap(), "./src/front/")
+}
+
 fn main() {
-
-    rouille::start_server("localhost:8000", move |request|
-        rouille::match_assets(&request, "./src/front/")
-    );
-
+    rouille::start_server("localhost:8000", move |request| {
+        router!(request,
+            (GET) ["/static/index.html"] => { handler(&request) },
+            (GET) ["/static/bundle.js"] => { handler(&request) },
+            (GET) ["/"] => { rouille::Response::redirect_302("/static/index.html") },
+            _ => rouille::Response::empty_404()
+            )
+        });
 }
