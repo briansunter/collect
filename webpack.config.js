9bda2c446a8ffb7a842813b397af8548fdebade9
IMPERIALE <alexandre.imperiale@cea.fr>
2018-03-19 10:18:57 +0100

    Fixing style loader and package dependencies.

--- webpack.config.js
+++ webpack.config.js
@@ -10,17 +10,19 @@ module.exports = {
   module: {
     rules: [
         {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
             options: { presets: ["env", "react"] }
           }
-        },
-        {
-          test: /\.css$/,
-          use: [ 'css-loader' ]
         }
       ]
   }
-};
+}
+
